c376fead9641a0ae3e9050755fad3b73f3291278
sujit gargate <sujitgargate5893@gmail.com>
2020-07-13 20:35:47 +0530

    Password Verification added

--- Patterns.sh
+++ Patterns.sh
@@ -68,3 +68,17 @@ else
 fi
 
 ## End of File
+
+## Password Verification
+
+#!/bin/bash -x
+echo "Enter password for checking"
+read s
+if [[ ${#s} -ge 6 && "$s" == *[[:upper:]]* && "$s" == *[[:lower:]]* && "$s" == *[0-9]* && "$s" == *[!@#%^*]* ]]
+then
+                echo "Matched"
+else
+                echo "failed"
+fi
+
+## End of File
