a4317192eb7dbee6eaefb0da676dcb23ad03cabc
sujit gargate <sujitgargate5893@gmail.com>
2020-07-13 20:34:21 +0530

    Mobile Number Validation added

--- Patterns.sh
+++ Patterns.sh
@@ -53,3 +53,18 @@ else
 fi
 
 ## End Of File
+
+
+## Mobile validation
+
+#!/bin/bash -x
+read -p "Enter Mobile Number " mobileNumber
+pattern="^[0-9]{1,2}[ ][0-9]{10}$"
+if [[ $mobileNumber =~ $pattern ]]
+then
+        echo "Valid Mobile Number"
+else
+        echo "Invalid Mobile Number"
+fi
+
+## End of File
