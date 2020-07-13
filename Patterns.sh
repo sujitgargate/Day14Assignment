b3f8e9121cf3c6691b97e08809ab0163bbc903c5
sujit gargate <sujitgargate5893@gmail.com>
2020-07-13 20:29:56 +0530

    First name validation added

--- Patterns.sh
+++ Patterns.sh
@@ -1,3 +1,20 @@
 ## Welcome Message ##
 echo "Welcome To Master Branch"
 ## End of Welcome Message ##
+
+##
+## First name Validation
+
+#!/bin/bash -x
+read -p "Enter a name  " name
+pattern=^[[:upper:]]{1}[a-z]{2,}$
+
+if [[ $name =~ $pattern ]]
+then
+        echo "Valid pattern"
+else
+        echo "Invalid Pattern"
+fi
+
+##End of File
+##
