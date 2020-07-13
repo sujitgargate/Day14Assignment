b770cb47d986b20363cdc9e4dc0da15e4418e44c
sujit gargate <sujitgargate5893@gmail.com>
2020-07-13 20:31:16 +0530

    Last name validation added

--- Patterns.sh
+++ Patterns.sh
@@ -18,3 +18,18 @@ fi
 
 ##End of File
 ##
+
+## Last name Validation
+
+#!/bin/bash -x
+pattern=^[[:upper:]]{1}[a-z]{2,}
+read -p "Enter last name  " last
+
+if [[ $last =~ $pattern ]]
+then
+        echo "Valid Last Name"
+else
+        echo "Invalid Last Name"
+fi
+
+## End of File
