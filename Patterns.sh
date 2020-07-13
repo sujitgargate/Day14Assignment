5e8107d119389a4aaac4d60990404758b9a3e19d
sujit gargate <sujitgargate5893@gmail.com>
2020-07-13 20:32:54 +0530

    Email Validation added

--- Patterns.sh
+++ Patterns.sh
@@ -33,3 +33,23 @@ else
 fi
 
 ## End of File
+
+### Email Validation
+
+#!/bin/bash -x
+echo "Enter Email : "
+read email
+
+pattern="^[0-9a-zA-Z]+([._+-][0-9a-zA-Z]+)*[@][0-9A-Za-z]+[.][a-zA-Z]{2,4}([.][a-zA-Z]{2})*$"
+# + => one or more occurances
+# ? => zero or one occurance
+# * => zero or more occurances
+
+if [[ $email =~ $pattern ]]
+then
+        echo "valid Email format"
+else
+        echo "Invalid email format"
+fi
+
+## End Of File
